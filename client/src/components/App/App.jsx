e1cb66aabbddf2839486e60b6cd4f3dbaac3c5c7
Teller35 <tellerwetzel@yahoo.com>
2021-07-28 19:00:11 -0600

    working on getting one sever

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,10 +1,16 @@
-import './App.css';
+import "./App.css";
+import { ApolloProvider } from "@apollo/reach-hooks";
+import ApolloClient from "apollo-boost";
+
+const client = new ApolloClient({
+  uri: "/graphql",
+});
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <ApolloProvider client={client}>
+      <div>Hello</div>
+    </ApolloProvider>
   );
 }
 
