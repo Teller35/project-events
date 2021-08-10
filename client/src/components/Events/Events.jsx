b27f3a8d6edf9cf889dbfefcdf0b790b72ca52e0
Teller35 <tellerwetzel@yahoo.com>
2021-08-10 16:20:57 -0600

    added addReaction file with all logic still now working fully

--- client/src/components/Events/Events.jsx
+++ client/src/components/Events/Events.jsx
@@ -1,36 +1,62 @@
-import React, { useEffect } from "react";
-import { Card, Row, Button, Col, CardGroup } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Card, Row, Button, Col, Modal } from "react-bootstrap";
+import ReactionForm from "../AddReaction";
 import { Link } from "react-router-dom";
-import "./Events.css"
+import "./Events.css";
+import AddReaction from "../AddReaction";
 
 const Events = ({ meetings, title }) => {
+  const [showForm, setShowForm] = useState(false);
+
   if (!meetings.length) {
     return <h2>No events yet...</h2>;
   }
-
   return (
-  <div className="m-2 border border-5 rounded">
-    <h3>{title}</h3>
-    <Row xs={1} md={2} lg={3}>
-    {meetings && 
-    meetings.map((meeting) => (
-      <Col className="p-2">
-      <Card style={{ width: "20rem" }} className="Border m-auto" key={meeting._id}>
-        <Card.Body className="Border">
-          <Card.Title className="text-center">{meeting.meetingType}</Card.Title>
-          <Card.Subtitle className="text-center">{meeting.reactionCount}</Card.Subtitle>
-          <Card.Text className="text-center">
-            Come join me at {meeting.place} for my {meeting.meetingType} event it 
-            will held at {meeting.meetingTime}.
-          </Card.Text>
-            <Button>R.S.V.P.</Button>
-        </Card.Body>
-      </Card>
-      </Col>
-      ))}
+    <div className="m-2 border border-5 rounded">
+      <h3>{title}</h3>
+      <Row xs={1} md={2} lg={3}>
+        {meetings &&
+          meetings.map((meeting) => (
+            <Col className="p-2">
+              <Card
+                style={{ width: "20rem" }}
+                className="Border m-auto"
+                key={meeting._id}
+              >
+                <Card.Body className="Border">
+                  <Card.Title className="text-center">
+                    {meeting.meetingType}
+                  </Card.Title>
+                  <Card.Subtitle className="text-center">
+                    Comments: {meeting.reactionsCount}
+                  </Card.Subtitle>
+                  <Card.Text className="text-center">
+                    Come join me at {meeting.place} for my {meeting.meetingType}{" "}
+                    event it will held at {meeting.meetingTime}.
+                  </Card.Text>
+                  <Button onClick={() => setShowForm(true)}>R.S.V.P.</Button>
+                  <Modal
+                    show={showForm}
+                    onHide={() => setShowForm(false)}
+                    backdrop="static"
+                    keyboard={false}
+                  >
+                    <Modal.Header closeButton>
+                      <Modal.Title>
+                        R.S.V.P. by leaving a comment...
+                      </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <AddReaction handleModalClose={() => setShowForm(false)} />
+                    </Modal.Body>
+                  </Modal>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
       </Row>
-  </div>
-  )
+    </div>
+  );
 };
 
 export default Events;
