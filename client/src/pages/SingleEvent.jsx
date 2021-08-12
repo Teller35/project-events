84adb33f15bc0290610ed595b38352fa35b88b0f
Teller35 <tellerwetzel@yahoo.com>
2021-08-11 18:39:54 -0600

    little card styling

--- client/src/pages/SingleEvent.jsx
+++ client/src/pages/SingleEvent.jsx
@@ -24,48 +24,52 @@ const SingleMeeting = (props) => {
   return (
     <div>
       {loggedIn && (
-      <Col className="p-2">
-        <Card
-          style={{ width: "auto" }}
-          className="Border m-auto"
-        >
-          <Card.Body className="Border">
-          <Link to="/home">
-            <CloseButton />
-            </Link>
-            <Card.Title className="text-center fs-1">
-              {meeting.meetingType}
-            </Card.Title>
-            <Card.Subtitle className="text-center fs-4">
-              Brought to you by: {meeting.username}
-            </Card.Subtitle>
-            <Card.Text className="text-center fs-5">
-              Come join me at <span className="fw-bold">{meeting.place} in {meeting.city}</span> for my <span className="fw-bold">{meeting.meetingType}</span> event
-              it will held at {meeting.date}.
-            </Card.Text>
-            <Card.Text className="text-center fs-4">
-                Comments:
-            </Card.Text>
-            <Card.Text className="text-center fs-6">
+        <Col className="p-2">
+          <Card style={{ width: "auto" }} className="Border2 m-auto">
+            <Card.Body className="Border2">
+              <Link to="/home">
+                <span className="MyRed">&larr; Back to Events</span>
+              </Link>
+              <Card.Title className="text-center fs-1">
+                {meeting.meetingType}
+              </Card.Title>
+              <Card.Subtitle className="text-center fs-4">
+                Brought to you by: {meeting.username}
+              </Card.Subtitle>
+              <Card.Text className="text-center fs-5">
+                Come join me at{" "}
+                <span className="fw-bold">
+                  {meeting.place} in {meeting.city}
+                </span>{" "}
+                for my <span className="fw-bold">{meeting.meetingType}</span>{" "}
+                event it will held at {meeting.date}.
+              </Card.Text>
+              <Card.Text className="text-center fs-4">Comments:</Card.Text>
+              <Card.Text className="text-center fs-6">
                 <Reactions reactions={meeting.reactions} />
-            </Card.Text>
-            <Button onClick={() => setShowForm(true)}>Leave a comment here</Button>
-            <Modal
-              show={showForm}
-              onHide={() => setShowForm(false)}
-              backdrop="static"
-              keyboard={false}
-            >
-              <Modal.Header closeButton>
-                <Modal.Title>R.S.V.P. by leaving a comment...</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                <AddReaction meetingId={meetingId} handleModalClose={() => setShowForm(false)} />
-              </Modal.Body>
-            </Modal>
-          </Card.Body>
-        </Card>
-      </Col>
+              </Card.Text>
+              <Button onClick={() => setShowForm(true)}>
+                Leave a comment here
+              </Button>
+              <Modal
+                show={showForm}
+                onHide={() => setShowForm(false)}
+                backdrop="static"
+                keyboard={false}
+              >
+                <Modal.Header closeButton>
+                  <Modal.Title>R.S.V.P. by leaving a comment...</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <AddReaction
+                    meetingId={meetingId}
+                    handleModalClose={() => setShowForm(false)}
+                  />
+                </Modal.Body>
+              </Modal>
+            </Card.Body>
+          </Card>
+        </Col>
       )}
     </div>
   );
