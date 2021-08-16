602d7bba6be89016d034b24dd085ee6c8a7ac2fa
Teller35 <tellerwetzel@yahoo.com>
2021-08-16 11:43:01 -0600

    Add Heroku

--- server/server.js
+++ server/server.js
@@ -20,6 +20,7 @@ async function startApolloServer() {
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
 
+
   if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../client/build")));
   }
